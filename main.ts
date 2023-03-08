9e6f228cda466f5daa59ff974590928cbb6ce9e6
Haoqi Shan Xu <104140318+al-7555056@users.noreply.github.com>
2023-03-08 08:51:26 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (Letra._pickRandom()))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(randint(1, 10))
+    basic.pause(1000)
+    basic.clearScreen()
+})
+let Letra: string[] = []
+Letra = [
+"A",
+"E",
+"I",
+"O",
+"U"
+]
